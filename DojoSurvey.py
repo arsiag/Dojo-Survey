27a32b96c763c6131312d5ed89d570f8de8d1353
Arsia Ghahari <arsia.ghahari@gmail.com>
2017-09-10 09:18:46 -0700

    second commit. Added form validation

--- DojoSurvey.py
+++ DojoSurvey.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
+
 app = Flask(__name__)
-# our index route will handle rendering our form
+app.secret_key = 'FormValidation'
+
 @app.route('/')
 def index():
+  location_list = ['San Jose', 'New York', 'Seattle', 'Dallas']
+  language_list = ['Python', 'PHP', 'Javascript', 'Ruby']
+  session['locations'] = location_list
+  session['languages'] = language_list
   return render_template("index.html")
 
-@app.route('/results', methods=['POST'])
-def process_survey():
-   
-   thisname = request.form['name']
-   thisloc = request.form['location']
-   thislang = request.form['lang']
-   thiscmnt = request.form['comment']
-   print "Dojo Survey results:"
-   print "Name: " + thisname
-   print "Loca: " + thisloc
-   print "Lang: " + thislang
-   print "Cmnt: " + thiscmnt
-   return render_template("results.html", name=thisname, location=thisloc, lang=thislang, comment=thiscmnt)
-   #return redirect('/')
-   
+@app.route('/verify', methods=['POST'])
+def verify_user():
+  if len(request.form['name']) == 0:
+      flash('Name cannot be blank', 'nameError')
+      return redirect('/')
+  if len(request.form['comment']) == 0:
+      flash('Comment cannot be blank', 'commentError')
+      return redirect('/')
+  if len(request.form['comment']) > 120:
+      flash('Comment is too long', 'commentError')
+      return redirect('/')
+
+  session['name'] = request.form['name']
+  session['location'] = request.form['location']
+  session['lang'] = request.form['lang']
+  session['comment'] = request.form['comment']
+
+  return redirect('/results')
+
+@app.route('/results')
+def show_results():
+  return render_template("results.html")
+
 app.run(debug=True) # run our server
